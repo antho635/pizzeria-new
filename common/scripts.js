da1643dc6c70c2fbcc60ddaacaad14286237d540
toutchykipik <56234761+toutchykipik@users.noreply.github.com>
2020-09-21 23:26:51 +0200

    btn back to top ok

--- common/scripts.js
+++ common/scripts.js
@@ -1,138 +1,137 @@
 function selectedActive(elem) {
-        var selectedAttr = $(elem),
-                selectionID = "." + $(selectedAttr).data("select");
+    var selectedAttr = $(elem),
+        selectionID = "." + $(selectedAttr).data("select");
 
-        $(".selecton a").removeClass("active");
-        $(selectedAttr).addClass("active");
-        $(".food-menu").removeClass("active");
+    $(".selecton a").removeClass("active");
+    $(selectedAttr).addClass("active");
+    $(".food-menu").removeClass("active");
 
-        if (selectionID == ".*") $(".food-menu").addClass("active");
-        else $(selectionID).addClass("active");
+    if (selectionID == ".*") $(".food-menu").addClass("active");
+    else $(selectionID).addClass("active");
 }
 
 
 function enableCounterUp(a) {
-        var counterElement;
-        if (isExists('#counter')) {
-                counterElement = $('#counter');
-        }
-        else {
-                return;
-        }
-        var oTop = $('#counter').offset().top - window.innerHeight;
-        if (a == 0 && $(window).scrollTop() > oTop) {
-                $('.counter-value').each(function () {
-                        var $this = $(this),
-                                countDuration = $this.data('duration'),
-                                countTo = $this.attr('data-count');
-                        $({countNum: $this.text()}).animate({countNum: countTo}, {
-                                duration: countDuration,
-                                easing: 'swing',
-                                step: function () {
-                                        $this.text(Math.floor(this.countNum));
-                                },
-                                complete: function () {
-                                        $this.text(this.countNum);
-                                }
-                        });
-                });
-                a = 1;
-        }
-        return a;
+    var counterElement;
+    if (isExists('#counter')) {
+        counterElement = $('#counter');
+    } else {
+        return;
+    }
+    var oTop = $('#counter').offset().top - window.innerHeight;
+    if (a == 0 && $(window).scrollTop() > oTop) {
+        $('.counter-value').each(function() {
+            var $this = $(this),
+                countDuration = $this.data('duration'),
+                countTo = $this.attr('data-count');
+            $({ countNum: $this.text() }).animate({ countNum: countTo }, {
+                duration: countDuration,
+                easing: 'swing',
+                step: function() {
+                    $this.text(Math.floor(this.countNum));
+                },
+                complete: function() {
+                    $this.text(this.countNum);
+                }
+            });
+        });
+        a = 1;
+    }
+    return a;
 }
 
-function panelAccordian(){
+function panelAccordian() {
 
-        var panelTitle = $('.panel-title');
-        panelTitle.on('click', function(){
-                $(this).toggleClass('active');
-                return false;
+    var panelTitle = $('.panel-title');
+    panelTitle.on('click', function() {
+        $(this).toggleClass('active');
+        return false;
+    });
+
+}
+
+
+function enableRadialProgress() {
+    $(".radial-progress").each(function() {
+        var $this = $(this),
+            progPercent = $this.data('prog-percent');
+
+        var bar = new ProgressBar.Circle(this, {
+            color: '#fff',
+            strokeWidth: 3,
+            trailWidth: 0,
+            easing: 'easeInOut',
+            duration: 1400,
+            text: {},
+            from: { color: '#fff', width: 1 },
+            to: { color: '#EF002E', width: 3 },
+            // Set default step function for all animate calls
+            step: function(state, circle) {
+                circle.path.setAttribute('stroke', state.color);
+                circle.path.setAttribute('stroke-width', state.width);
+
+                var value = Math.round(circle.value() * 100);
+                if (value === 0) { circle.setText(''); } else { circle.setText(value); }
+            }
         });
 
+        $(this).waypoint(function() { bar.animate(progPercent); }, { offset: "90%" })
+    });
 }
 
+(function($) {
 
-function enableRadialProgress(){
-        $(".radial-progress").each(function(){
-                var $this = $(this),
-                        progPercent = $this.data('prog-percent');
+    "use strict";
 
-                var bar = new ProgressBar.Circle(this, {
-                        color: '#fff',
-                        strokeWidth: 3,
-                        trailWidth: 0,
-                        easing: 'easeInOut',
-                        duration: 1400,
-                        text: {},
-                        from: { color: '#fff', width: 1 },
-                        to: { color: '#EF002E', width: 3 },
-                        // Set default step function for all animate calls
-                        step: function(state, circle) {
-                                circle.path.setAttribute('stroke', state.color);
-                                circle.path.setAttribute('stroke-width', state.width);
+    // ACCORDIAN
 
-                                var value = Math.round(circle.value() * 100);
-                                if (value === 0) { circle.setText(''); } else { circle.setText(value); }
-                        }
-                });
+    panelAccordian();
 
-                $(this).waypoint(function(){ bar.animate(progPercent); },{offset: "90%"})
-        });
-}
+    // RADIAL PROGRESS BAR
+    enableRadialProgress();
 
-(function ($) {
+    enableSwiper();
 
-        "use strict";
+    /*COUNTER*/
+    var countLineProgress = 0;
+    var countCounterUp = 0;
+    var a = 0;
+    countCounterUp = enableCounterUp(countCounterUp);
 
-        // ACCORDIAN
-
-        panelAccordian();
-
-        // RADIAL PROGRESS BAR
-        enableRadialProgress();
-
-        enableSwiper();
-
-        /*COUNTER*/
-        var countLineProgress = 0;
-        var countCounterUp = 0;
-        var a = 0;
+    $(window).on('scroll', function() {
         countCounterUp = enableCounterUp(countCounterUp);
+    });
 
-        $(window).on('scroll', function () {
-                countCounterUp = enableCounterUp(countCounterUp);
-        });
+    /*CUSTOME ISOTOPE*/
+    var selectedAttr = $('[data-select].active');
+    selectedActive(selectedAttr);
 
-        /*CUSTOME ISOTOPE*/
-        var selectedAttr = $('[data-select].active');
+    $('[data-select]').on("click", function() {
+        var selectedAttr = $(this);
         selectedActive(selectedAttr);
-
-        $('[data-select]').on("click", function () {
-                var selectedAttr = $(this);
-                selectedActive(selectedAttr);
-                return false;
-        });
+        return false;
+    });
 
 
-        // DROPDOWN MENU
+    // DROPDOWN MENU
 
-        var winWidth = $(window).width();
+    var winWidth = $(window).width();
+    dropdownMenu(winWidth);
+
+    $(window).on('resize', function() {
+        winWidth = $(window).width();
         dropdownMenu(winWidth);
 
-        $(window).on('resize', function () {
-                winWidth = $(window).width();
-                dropdownMenu(winWidth);
-
-        });
+    });
 
 
-        $('[data-menu]').on('click', function () {
+    $('[data-menu]').on('click', function() {
 
-                var mainMenu = $(this).data('menu');
+        var mainMenu = $(this).data('menu');
 
-                $(mainMenu).toggleClass('visible-menu');
+        $(mainMenu).toggleClass('visible-menu');
 
-        });
+    });
 
 
 })(jQuery);
@@ -140,124 +139,142 @@ function enableRadialProgress(){
 
 function dropdownMenu(winWidth) {
 
-        if (winWidth > 767) {
+    if (winWidth > 767) {
 
-                $('.main-menu li.drop-down').on('mouseover', function () {
-                        var $this = $(this),
-                                menuAnchor = $this.children('a');
+        $('.main-menu li.drop-down').on('mouseover', function() {
+            var $this = $(this),
+                menuAnchor = $this.children('a');
 
-                        menuAnchor.addClass('mouseover');
+            menuAnchor.addClass('mouseover');
 
-                }).on('mouseleave', function () {
-                        var $this = $(this),
-                                menuAnchor = $this.children('a');
+        }).on('mouseleave', function() {
+            var $this = $(this),
+                menuAnchor = $this.children('a');
 
-                        menuAnchor.removeClass('mouseover');
-                });
+            menuAnchor.removeClass('mouseover');
+        });
 
-        } else {
+    } else {
 
-                $('.main-menu li.drop-down > a').on('click', function () {
+        $('.main-menu li.drop-down > a').on('click', function() {
 
-                        if ($(this).attr('href') == '#') return false;
-                        if ($(this).hasClass('mouseover')) {
-                                $(this).removeClass('mouseover');
-                        }
-                        else {
-                                $(this).addClass('mouseover');
-                        }
-                        return false;
-                });
-        }
+            if ($(this).attr('href') == '#') return false;
+            if ($(this).hasClass('mouseover')) {
+                $(this).removeClass('mouseover');
+            } else {
+                $(this).addClass('mouseover');
+            }
+            return false;
+        });
+    }
 
 }
 
 function enableSwiper() {
 
-        if (isExists('.swiper-container')) {
+    if (isExists('.swiper-container')) {
 
-                $('.swiper-container').each(function (index) {
+        $('.swiper-container').each(function(index) {
 
-                        var swiperDirection = $(this).data('swiper-direction'),
-                                swiperSlidePerView = $(this).data('swiper-slides-per-view'),
-                                swiperBreakpoints = $(this).data('swiper-breakpoints'),
-                                swiperSpeed = $(this).data('swiper-speed'),
-                                swiperCrossFade = $(this).data('swiper-crossfade'),
-                                swiperLoop = $(this).data('swiper-loop'),
-                                swiperAutoplay = $(this).data('swiper-autoplay'),
-                                swiperMousewheelControl = $(this).data('swiper-wheel-control'),
-                                swipeSlidesPerview = $(this).data('slides-perview'),
-                                swiperMargin = parseInt($(this).data('swiper-margin')),
-                                swiperSlideEffect = $(this).data('slide-effect'),
-                                swiperAutoHeight = $(this).data('autoheight'),
-                                swiperScrollbar = ($(this).data('scrollbar') ? $(this).parentsUntil('.swiper-area').find('.swiper-scrollbar') : null);
-                        swiperScrollbar = (isExists(swiperScrollbar) ? swiperScrollbar : null),
-                                swprResponsive = $(this).data('swpr-responsive');
+            var swiperDirection = $(this).data('swiper-direction'),
+                swiperSlidePerView = $(this).data('swiper-slides-per-view'),
+                swiperBreakpoints = $(this).data('swiper-breakpoints'),
+                swiperSpeed = $(this).data('swiper-speed'),
+                swiperCrossFade = $(this).data('swiper-crossfade'),
+                swiperLoop = $(this).data('swiper-loop'),
+                swiperAutoplay = $(this).data('swiper-autoplay'),
+                swiperMousewheelControl = $(this).data('swiper-wheel-control'),
+                swipeSlidesPerview = $(this).data('slides-perview'),
+                swiperMargin = parseInt($(this).data('swiper-margin')),
+                swiperSlideEffect = $(this).data('slide-effect'),
+                swiperAutoHeight = $(this).data('autoheight'),
+                swiperScrollbar = ($(this).data('scrollbar') ? $(this).parentsUntil('.swiper-area').find('.swiper-scrollbar') : null);
+            swiperScrollbar = (isExists(swiperScrollbar) ? swiperScrollbar : null),
+                swprResponsive = $(this).data('swpr-responsive');
 
-                        var swiper = new Swiper($(this)[0], {
-                                pagination: $(this).find('.swiper-pagination'),
-                                slidesPerView: (swiperSlidePerView ? swiperSlidePerView : 1),
-                                direction: (swiperDirection ? swiperDirection : 'horizontal'),
-                                loop: (swiperLoop ? swiperLoop : false),
-                                nextButton: '.swiper-button-next',
-                                prevButton: '.swiper-button-prev',
-                                autoplay: (swiperAutoplay ? swiperAutoplay : false),
-                                paginationClickable: true,
-                                spaceBetween: (swiperMargin ? swiperMargin : 0),
-                                mousewheelControl: ((swiperMousewheelControl) ? swiperMousewheelControl : false),
-                                scrollbar: (swiperScrollbar ? swiperScrollbar : null),
-                                scrollbarHide: false,
-                                speed: (swiperSpeed ? swiperSpeed : 1000),
-                                autoHeight: ((swiperAutoHeight == false) ? swiperAutoHeight : true),
-                                effect: (swiperSlideEffect ? swiperSlideEffect : 'coverflow'),
-                                fade: {crossFade: swiperCrossFade ? swiperCrossFade : false},
-                                breakpoints: {
-                                        1200: {slidesPerView: swprResponsive[3] ? swprResponsive[3] : 1,},
-                                        992: {slidesPerView: swprResponsive[2] ? swprResponsive[2] : 1,},
-                                        768: {slidesPerView: swprResponsive[1] ? swprResponsive[1] : 1,},
-                                        576: {slidesPerView: swprResponsive[0] ? swprResponsive[0] : 1,}
+            var swiper = new Swiper($(this)[0], {
+                pagination: $(this).find('.swiper-pagination'),
+                slidesPerView: (swiperSlidePerView ? swiperSlidePerView : 1),
+                direction: (swiperDirection ? swiperDirection : 'horizontal'),
+                loop: (swiperLoop ? swiperLoop : false),
+                nextButton: '.swiper-button-next',
+                prevButton: '.swiper-button-prev',
+                autoplay: (swiperAutoplay ? swiperAutoplay : false),
+                paginationClickable: true,
+                spaceBetween: (swiperMargin ? swiperMargin : 0),
+                mousewheelControl: ((swiperMousewheelControl) ? swiperMousewheelControl : false),
+                scrollbar: (swiperScrollbar ? swiperScrollbar : null),
+                scrollbarHide: false,
+                speed: (swiperSpeed ? swiperSpeed : 1000),
+                autoHeight: ((swiperAutoHeight == false) ? swiperAutoHeight : true),
+                effect: (swiperSlideEffect ? swiperSlideEffect : 'coverflow'),
+                fade: { crossFade: swiperCrossFade ? swiperCrossFade : false },
+                breakpoints: {
+                    1200: { slidesPerView: swprResponsive[3] ? swprResponsive[3] : 1, },
+                    992: { slidesPerView: swprResponsive[2] ? swprResponsive[2] : 1, },
+                    768: { slidesPerView: swprResponsive[1] ? swprResponsive[1] : 1, },
+                    576: { slidesPerView: swprResponsive[0] ? swprResponsive[0] : 1, }
 
-                                },
-                        });
+                },
+            });
 
-                        $('.swiper-container').hover(function () {
-                                swiper.stopAutoplay();
-                        }, function () {
-                                swiper.startAutoplay();
-                        });
-
-
-                });
+            $('.swiper-container').hover(function() {
+                swiper.stopAutoplay();
+            }, function() {
+                swiper.startAutoplay();
+            });
 
 
-        }
+        });
+
+
+    }
 }
 
 function isExists(elem) {
-        if ($(elem).length > 0) {
-                return true;
-        }
-        return false;
+    if ($(elem).length > 0) {
+        return true;
+    }
+    return false;
 }
 
 function initMap() {
 
-        // Create a map object, and include the MapTypeId to add
-        // to the map type control.
+    // Create a map object, and include the MapTypeId to add
+    // to the map type control.
 
-        var uluru = {lat: 56.946285, lng: 24.105078};
-        var map = new google.maps.Map(document.getElementById('map'), {
-                zoom: 4,
-                center: uluru
-        });
+    var uluru = { lat: 56.946285, lng: 24.105078 };
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: uluru
+    });
 
-        var image = 'images/google-marker.png';
-        var marker = new google.maps.Marker({
-                position: uluru,
-                map: map,
-                icon: image
-        });
-        //Associate the styled map with the MapTypeId and set it to display.
-        map.mapTypes.set('styled_map', styledMapType);
-        map.setMapTypeId('styled_map');
+    var image = 'images/google-marker.png';
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map,
+        icon: image
+    });
+    //Associate the styled map with the MapTypeId and set it to display.
+    map.mapTypes.set('styled_map', styledMapType);
+    map.setMapTypeId('styled_map');
+}
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
